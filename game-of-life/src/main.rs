465a007de64cecff757f4db5ef48acade7eb17e5
Ricardo Serrato B <rsbse@hotmail.com>
2025-10-08 06:45:26 -0600

    Game of life

--- game-of-life/src/main.rs
+++ game-of-life/src/main.rs
@@ -1,10 +1,11 @@
 use macroquad::{prelude::*, rand::RandomRange};
 
-const SIZE: usize = 10;
-const SQUARE_SIZE: f32 = 10.;
+const SIZE: usize = 200;
+const SQUARE_SIZE: f32 = 5.;
+const DELAY: f64 = 0.2;
 
 struct World {
-    cells: Vec<Vec<u16>>,
+    cells: Vec<u16>,
 }
 
 impl World {
@@ -14,17 +15,14 @@ impl World {
         }
     }
 
-    fn create_cells() -> Vec<Vec<u16>> {
-        let mut cells: Vec<Vec<u16>> = Vec::new();
+    fn create_cells() -> Vec<u16> {
+        let mut cells: Vec<u16> = Vec::new();
 
-        for j in 0..SIZE {
-            let mut v: Vec<u16> = Vec::new();
-            for i in 0..SIZE {
-                v.push(World::random_binary());
-            }
-            cells.push(v);
+        for _ in 0..SIZE * SIZE {
+            cells.push(World::random_binary());
         }
 
+        // cells[0] = 1;
         return cells;
     }
 
@@ -40,7 +38,6 @@ impl World {
 
     fn draw_cell(&self, i: usize, j: usize, is_alive: u16) {
         let color = if is_alive == 1 { WHITE } else { BLACK };
-
         draw_rectangle(
             i as f32 * SQUARE_SIZE,
             j as f32 * SQUARE_SIZE,
@@ -53,7 +50,7 @@ impl World {
     fn draw(&self) {
         for j in 0..SIZE {
             for i in 0..SIZE {
-                let cell = self.cells[j][i];
+                let cell = self.cells[j * SIZE + i];
                 self.draw_cell(i, j, cell);
             }
         }
@@ -61,69 +58,90 @@ impl World {
 
     fn count_neighbors(&self, i: usize, j: usize) -> u16 {
         let mut sum: u16 = 0;
-        for x in i - 1..i + 2 {
-            for y in j - 1..j + 2 {
+        let x_from = i.saturating_sub(1);
+        let x_to = (i + 2).min(SIZE);
 
-                if x == i {continue;}
-                if y == j {continue;}
+        let y_from = j.saturating_sub(1);
+        let y_to = (j + 2).min(SIZE);
 
-                sum = sum + self.get_cell_value(i, j);
+        for y in y_from..y_to {
+            for x in x_from..x_to {
+                if x == i && y == j {
+                    continue;
+                }
+
+                let value = self.cells[y * SIZE + x];
+                sum += value;
             }
         }
 
         return sum;
     }
 
-    fn get_cell_value(&self, i:usize, j:usize) -> u16 {
-        if self.cells.len() -1 > j {
-            return 0;
+    fn get_copy(&self) -> Vec<u16> {
+        let mut new: Vec<u16> = Vec::new();
+
+        for e in &self.cells {
+            new.push(e.clone());
         }
 
-        if self.cells[j].len() - 1 > i {
-            return 0;
-        }
-
-        return self.cells[j][i];
-
+        return new;
     }
 
-    fn next_state_cells(&self) {
-        let mut copy = self.cells.clone();
+    fn next_state_cells(&mut self) {
+        let mut copy = self.get_copy();
+        // println!("Cells: {:?}", self.cells);
 
         for j in 0..SIZE {
             for i in 0..SIZE {
                 let count = self.count_neighbors(i, j);
-                let new_state = self.next_state_cell(count); 
+                let is_alive = self.cells[j * SIZE + i];
+                let new_state = self.next_state_cell(count, is_alive);
+
+                // println!("State {}, {}, {}, {}", i, j, count, new_state);
+                copy[j * SIZE + i] = new_state;
             }
         }
+
+        self.cells = copy;
     }
 
-    fn next_state_cell(&self, n_count: u16) -> u16 {
+    fn next_state_cell(&self, n_count: u16, is_alive: u16) -> u16 {
+        if is_alive == 1 {
+            if n_count < 2 {
+                return 0;
+            }
 
-        if n_count > 3 {
-            return 0;
+            if n_count <= 3 {
+                return 1;
+            } else {
+                return 0;
+            }
         }
-        
+
         if n_count == 3 {
             return 1;
         }
 
-         if n_count < 2 {
-            return 0;
-        }
-        return 0;
+        return is_alive;
     }
-
-
 }
 
 #[macroquad::main("MyGame")]
 async fn main() {
-    let world = World::new();
+    let mut world = World::new();
+    let mut time = get_time();
+
     loop {
         clear_background(BLACK);
 
         world.draw();
+
+        if get_time() - time > DELAY {
+            world.next_state_cells();
+            time = get_time();
+        }
+
         next_frame().await
     }
 }
