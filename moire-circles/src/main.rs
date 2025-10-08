eaac14ce1f56b8f8255f86999061b45e20f9035b
Ricardo Serrato B <rsbse@hotmail.com>
2025-10-08 10:58:54 -0600

    Moire circles

--- moire-circles/src/main.rs
+++ moire-circles/src/main.rs
@@ -1,35 +1,34 @@
-use std::arch::x86_64::_mm_andnot_si128;
-
 use macroquad::prelude::*;
 
-fn draw_circles(angle: f32, radius: f32) {
+fn draw_circles(angle: f32, radius: f32, color: Color) {
     let x = radius * angle.to_radians().cos();
     let y = radius * angle.to_radians().sin();
+
     let cx = screen_width() / 2.;
     let cy = screen_height() / 2.;
+
     let mut r = radius;
 
     while r > 0. {
-
-        draw_circle_lines(x + cx, y + cy, r, 1., WHITE);
-        r = r - 5.
-
+        draw_circle_lines(x + cx, y + cy, r, 2., color);
+        r = r - 5.;
     }
 }
 
 #[macroquad::main("MyGame")]
 async fn main() {
-
-    let mut angle: f32 = 0.;
     let radius: f32 = 200.;
+    let mut angle: f32 = 0.;
 
     loop {
         clear_background(BLACK);
 
-        draw_circles(angle, radius);
-        draw_circles(angle*-1., radius);
+        draw_circles(angle, radius, GREEN);
+        draw_circles(angle+45., radius, YELLOW);
+        draw_circles((angle+45.) * -1., radius, PINK);
+        draw_circles(angle * -1., radius, BLUE);
 
-        angle += 1.;
+        angle = angle + 1.;
 
         next_frame().await
     }
