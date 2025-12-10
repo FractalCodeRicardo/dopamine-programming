fd8138aee7d94d7a13b87488d06ed6f9fb54110a
Ricardo Serrato B. <rsbse@hotmail.com>
2025-12-10 15:50:17 -0600

    Random walk changes

--- random-walk/src/main.rs
+++ random-walk/src/main.rs
@@ -1,79 +1,89 @@
 use macroquad::{prelude::*, rand::RandomRange};
 
-const SQUARE_SIZE: f32 = 5.;
-const  STEP: f32 = 6.;
+const  SIZE: f32  =  10.;
+const  STEP: f32  =  11.;
+
+
 struct Agent {
-    color: Color,
-    path: Vec<Vec2>
+    path: Vec<Vec2>,
+    color: Color
 }
 
 impl Agent {
+
     fn new(color: Color) -> Self {
-        let mut agent = Agent{
-            color: color,
-            path: vec![]
+
+        let mut agent  = Agent {
+            path: vec![],
+            color: color
         };
 
-        agent.path.push(vec2(screen_width() / 2., screen_height() / 2.));
+        agent.path.push(vec2(screen_width() /2., screen_height()/2.));
 
-        return agent
+        agent
     }
 
-    fn mov(&mut self) {
+
+    fn random_dir() -> Vec2 {
         let dirs = vec![
             vec2(STEP,0.),
             vec2(-STEP,0.),
+            vec2(0.,STEP),
             vec2(0.,-STEP),
-            vec2(0.,STEP)
         ];
 
-        let index = RandomRange::gen_range(0, dirs.len());
+        let i = RandomRange::gen_range(0, dirs.len());
 
-        let dir = dirs[index];
-
-        let n_pos  = dir + self.path[self.path.len() -1];
-        self.path.push(n_pos);
+        return dirs[i];
+        
     }
 
-    fn draw(&self) {
-        for i in 0..self.path.len() -1  {
-            let p1 = self.path[i];
-            let p2 = self.path[i + 1];
+    fn draw(&self)  {
+        for i in 0..self.path.len() -1 {
+            let p1  = self.path[i];
+            let p2  = self.path[i+1];
 
-            draw_line(p1.x, p1.y, p2.x, p2.y, 2., self.color);
+            draw_line(p1.x, p1.y, p2.x, p2.y, 3., self.color);
 
         }
 
+        let p = self.path[self.path.len() -1];
 
-        let p = self.path[self.path.len() - 1];
-        draw_rectangle(p.x, p.y, SQUARE_SIZE, SQUARE_SIZE, self.color);
+        draw_rectangle(p.x, p.y, SIZE, SIZE, self.color);
+    }
+
+    fn mov(&mut self) {
+        let dir  = Agent::random_dir();
+        let p = self.path[self.path.len() -1];
+
+
+        let n_p  = dir + p;
+
+        self.path.push(n_p);
     }
 }
-
-fn random_color() -> Color {
-    Color::new(
-        rand::gen_range(0.0, 1.0),
-        rand::gen_range(0.0, 1.0),
-        rand::gen_range(0.0, 1.0),
-        1.0,
-    )
-}
-
 #[macroquad::main("MyGame")]
 async fn main() {
-
     let mut agents = vec![];
 
+   //the simulation is done but lets make it more beauty 
     for i in 0..100 {
-        agents.push(Agent::new(random_color()));
-
+        agents.push(Agent::new(Color {
+            r: RandomRange::gen_range(0., 1.),
+            g: RandomRange::gen_range(0., 1.),
+            b: RandomRange::gen_range(0., 1.),
+            a:1., 
+        }));
     }
-    loop {
 
-        for a in &mut agents {
-            a.draw();
-            a.mov();
+    loop {
+        clear_background(BLACK);
+
+        for p in &mut agents {
+            p.draw();
+            p.mov();
         }
+
 
         next_frame().await
     }
